ba1a47b13a6f0d7b176c564da8d7b25e43e5a454
Semert <semert2121@gmail.com>
2020-10-19 12:30:51 +0300

    Custom Hook Api key

--- src/useGif.js
+++ src/useGif.js
@@ -1,2 +1,5 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
